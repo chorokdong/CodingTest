27f2092a4f5daf310b8bd9362c3047f108f4da6f
glad415 <103625261+chorokdong@users.noreply.github.com>
2023-01-05 18:43:48 +0900

    Add files via upload

--- Programmers/[Lv.2]_가격대별_상품_개수_구하기.sql
+++ Programmers/[Lv.2]_가격대별_상품_개수_구하기.sql
@@ -1,21 +1,7 @@
+---- https://school.programmers.co.kr/learn/courses/30/lessons/131530
+---- 가격대별 상품 개수 구하기
 
-select sum(ks."Close") as "close" , sum(ks."Change") as "change" , split_part(ks.date::varchar, '-', 1) as date, ss.value as sector 
-from kospi_sql ks 
-join sector_sql ss on ss.code = ks.code 
-group by 3,4
-order by 4,3
-
-select ks."Close" as "close" , ks."Change" as "change" , split_part(ks.date::varchar, '-', 1) as date, ss.value as sector, ks.code 
-from kospi_sql ks 
-join sector_sql ss on ss.code = ks.code 
-
-
-select sum(ks."Close") as "close" , sum(ks."Change") as "change" , split_part(ks.date::varchar, '-', 1) as date, ss.value as sector 
-from kosdaq_sql ks  
-join sector_sql ss on ss.code = ks.code 
-group by 3,4
-order by 4,3
-
-select ks."Close" as "close" , ks."Change" as "change" , split_part(ks.date::varchar, '-', 1) as date, ss.value as sector, ks.code 
-from kosdaq_sql ks 
-join sector_sql ss on ss.code = ks.code 
+SELECT TRUNCATE(PRICE, -4) AS PRICE_GROUP, COUNT(PRODUCT_ID)
+FROM product
+GROUP BY PRICE_GROUP
+ORDER BY PRICE_GROUP
